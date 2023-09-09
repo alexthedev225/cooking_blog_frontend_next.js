6b73b677cc840bb639262be5166bdac2a36ead72
alexthedev225 <alexcode225@gmail.com>
2023-09-09 13:27:13 +0000

    configuration de socket.io

--- components/DeleteArticleButton.jsx
+++ components/DeleteArticleButton.jsx
@@ -1,37 +1,35 @@
-"use client"
+"use client";
 import styles from "@/styles/Button.module.css";
 import { useCookies } from "react-cookie";
-import { useRouter } from "next/navigation"; // Importez le hook useRouter
-
-async function deleteArticle(articleId, token, router) {
-  try {
-    const response = await fetch(
-      `https://cooking-blog-backend-express-js.onrender.com/api/articles/${articleId}`,
-      {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`, // Ajout du jeton d'authentification
-        },
-      }
-    );
-
-    if (response.status === 200) {
-      // Suppression réussie, redirigez vers la page d'accueil
-      console.log("Article supprimé avec succès");
-      router.push("/"); // Redirection vers la page d'accueil
-    } else {
-      // La suppression a échoué, affichez un message d'erreur ou effectuez une autre action ici
-      console.error("Échec de la suppression de l'article");
-    }
-  } catch (error) {
-    console.error("Erreur lors de la suppression de l'article", error);
-  }
-}
+import { redirect } from "next/navigation"; // Importez le hook useRouter
 
 export default function DeleteArticleButton({ articleId }) {
   const [cookies] = useCookies(["token", "userId"]);
   const token = cookies.token;
-  const router = useRouter(); // Utilisez le hook useRouter
+  async function deleteArticle() {
+    try {
+      const response = await fetch(
+        `https://cooking-blog-backend-express-js.onrender.com/api/articles/${articleId}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`, // Ajout du jeton d'authentification
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        // Suppression réussie, redirigez vers la page d'accueil
+        console.log("Article supprimé avec succès");
+        redirect("/"); // Redirection vers la page d'accueil
+      } else {
+        // La suppression a échoué, affichez un message d'erreur ou effectuez une autre action ici
+        console.error("Échec de la suppression de l'article");
+      }
+    } catch (error) {
+      console.error("Erreur lors de la suppression de l'article", error);
+    }
+  }
 
   const handleDelete = () => {
     // Demande de confirmation de suppression à l'utilisateur si nécessaire
